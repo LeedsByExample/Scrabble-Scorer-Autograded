60e1ff401100cfb2d48a8c0b46111fa8b1cc519b
LeedsByExample <leeda.minkin@yahoo.com>
2023-10-09 12:28:24 -0500

    Completed Assignment 2: Scrabble Scorer. Was super jazzed about it.

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -26,34 +26,143 @@ function oldScrabbleScorer(word) {
  
 	  }
 	}
-	return letterPoints;
- }
+	return letterPoints; 
+}
 
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   console.log("Let's play some scrabble!");
+   let prompt = input.question("Enter a word: ");
+   return prompt;
 };
 
-let simpleScorer;
 
-let vowelBonusScorer;
+function simpleScorer(word) {
+   //Define a function that takes a word as a parameter and returns a numerical score. Each letter within the word is worth 1 point.
+   return word.length;
+};
+//console.log(simpleScorer("hello"));
 
-let scrabbleScorer;
+//Define a function that takes a word as a parameter and returns a score. Each vowel within the word is worth 3 points, 
+//and each consonant is worth 1 point.
+function vowelBonusScorer(word) {
+   let points = 0;
+   word = word.toLowerCase();
+   let splitLetters = word.split("");
+   for (let i = 0; i < splitLetters.length; i++) {
+      if (splitLetters[i] === "a" || splitLetters[i] === "e" || splitLetters[i] === "i" || splitLetters[i] === "o" || splitLetters[i] === "u") {
+         points += 3;
+      } else {
+         points += 1;
+      }
+   } 
+   return points;
+};
+//console.log(vowelBonusScorer("leeda"));
 
-const scoringAlgorithms = [];
+//let scrabbleScorer;
+function scrabbleScorer(word) {
+   word = word.toLowerCase();
+   let score = 0;
+   for (let i = 0; i < word.length; i++) {
+      score += Number(newPointStructure[word[i]]);
+   }
+   return score;
+};
 
-function scorerPrompt() {}
 
-function transform() {};
+//Finish writing the scoringAlgorithms array. It should be populated with three objects, one for each of the three scoring options. 
+//Each object should contain three keys: name, description, and scoringFunction (NAME THIS KEY scorerFunction INSTEAD)
 
-let newPointStructure;
+// let simpleScore = {
+//    name: "Simple Score",
+//    description: "Each letter is worth 1 point.",
+//    scorerFunction: simpleScorer
+// };
+// let vowelBonus = {
+//    name: "Bonus Vowels",
+//    description: "Vowels are 3 pts, consonants are 1 pt.",
+//    scorerFunction: vowelBonusScorer
+// };
+// let oldScrabble = {
+//    name: "Scrabble",
+//    description: "The traditional scoring algorithm.",
+//    scorerFunction: oldScrabbleScorer
+// };
+// const scoringAlgorithms = [simpleScore, vowelBonus, oldScrabble];
+//console.log(scoringAlgorithms);
+
+const scoringAlgorithms = [
+   {
+      name: "Simple Score",
+      description: "Each letter is worth 1 point.",
+      scorerFunction: simpleScorer
+   },
+   {
+      name: "Bonus Vowels",
+      description: "Vowels are 3 pts, consonants are 1 pt.",
+      scorerFunction: vowelBonusScorer
+   },
+   {
+      name: "Scrabble",
+      description: "The traditional scoring algorithm.",
+      scorerFunction: scrabbleScorer
+   }
+];
+//console.log(scoringAlgorithms);
+
+//Finish writing scorerPrompt() so that the user can select which scoring algorithm to use when the program scores their word. 
+//Use the selected algorithm to determine the score for the word
+//scorerPrompt() should return the object the user has selected.
+function scorerPrompt(word) {
+   let scorerSelection = input.question(`Which scoring algorithm would you like to use?
+   0 - ${scoringAlgorithms[0].name}: ${scoringAlgorithms[0].description}
+   1 - ${scoringAlgorithms[1].name}: ${scoringAlgorithms[1].description}
+   2 - ${scoringAlgorithms[2].name}: ${scoringAlgorithms[2].description}
+   Enter 0, 1, or 2: `);
+
+   while (scorerSelection < 0 || scorerSelection > 2) {
+      scorerSelection = input.question(`Which scoring algorithm would you like to use?
+   0 - ${scoringAlgorithms[0].name}: ${scoringAlgorithms[0].description}
+   1 - ${scoringAlgorithms[1].name}: ${scoringAlgorithms[1].description}
+   2 - ${scoringAlgorithms[2].name}: ${scoringAlgorithms[2].description}
+   Enter 0, 1, or 2: `);
+   };
+
+   if (scorerSelection === "0") {
+      return (`Score for "${word}": ${scoringAlgorithms[Number(scorerSelection)].scorerFunction(word)}`);
+   } else if (scorerSelection === "1") {
+      return (`Score for "${word}": ${scoringAlgorithms[Number(scorerSelection)].scorerFunction(word)}`)
+   } else if (scorerSelection === "2") {
+      return (`Score for "${word}": ${scoringAlgorithms[Number(scorerSelection)].scorerFunction(word)}`)
+   }; 
+}; 
+
+//transform function should take the oldPointStructure object as a parameter
+//calling transform(oldPointStructure) will return an object with LOWERCASE letters as keys
+//the value for each key will be the points assigned to that letter
+function transform(structure) {
+   let newPointStructure = {};
+   for (key in structure) {
+      for (i = 0; i < structure[key].length; i++) {
+         newPointStructure[structure[key][i].toLowerCase()] = Number(key);
+      }
+   }
+   return newPointStructure;
+};
+//   objectName["new-key"] = propertyValue;
+let newPointStructure = transform(oldPointStructure);
+
 
 function runProgram() {
-   initialPrompt();
-   
+   let prompt = initialPrompt();
+   console.log(scorerPrompt(prompt));
+   //let prompt = initialPrompt();
+   //console.log(scorerPrompt(prompt));
 }
+runProgram(); //don't call this?
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
@@ -69,3 +178,4 @@ module.exports = {
 	runProgram: runProgram,
 	scorerPrompt: scorerPrompt
 };
+
